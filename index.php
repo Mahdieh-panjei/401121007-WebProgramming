799f5bebf3b78cd8ad534f607efffa71cf4691c0
401121007 <mahdiehpanjei@gmail.com>
2025-05-26 23:54:29 +0330

    commit_3

--- index.php
+++ index.php
@@ -1,31 +1,53 @@
 <?php
 define('ROOT_PATH',__DIR__);
+require __DIR__.'/vendor/autoload.php';
 require_once(__DIR__.'/helper/functions.php');
-$method = $_SERVER['REQUEST_METHOD'];
-$uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+// require_once(__DIR__.'/app/Route.php');
+ 
+use App\Route;
+use App\Controller\FrontController;
 
-if($uri == "/webprogramming/")
-{
-    view('home.php');
+$route = new Route();
+$route->addRoute("GET","/webprogramming/",[FrontController::class, 'home']);
+$route->addRoute("GET",'/webprogramming/about',[FrontController::class, 'about']);
+$route->addRoute("GET",'/webprogramming/infs',[FrontController::class, 'infs']);
+$route->addRoute("POST",'/webprogramming/process-form',[FrontController::class, 'process']);
+$route->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-}else if($uri == '/webprogramming/infs')
-{
-    view('infs.php');
 
-}else if($uri == '/webprogramming/about')
-{
-    view('about.php');
 
-}
-if($uri == "/webprogramming/process-form")
-{
-    view('process-form.php');
 
-}
-else{
-    view('404.php');
+
+
+
+// <?php
+// define('ROOT_PATH',__DIR__);
+// require_once(__DIR__.'/helper/functions.php');
+// $method = $_SERVER['REQUEST_METHOD'];
+// $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+// if($uri == "/webprogramming/")
+// {
+//     view('home.php');
+
+// }else if($uri == '/webprogramming/infs')
+// {
+//     view('infs.php');
+
+// }else if($uri == '/webprogramming/about')
+// {
+//     view('about.php');
+
+// }
+// else if($uri == "/webprogramming/process-form")
+// {
+//     view('process-form.php');
+
+// }
+// else{
+//     view('404.php');
     
-}
+// }
 
 ?>
 
